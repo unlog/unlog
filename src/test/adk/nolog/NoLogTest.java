d4e1dc4f13e418a6b3fee6855e2db609ff1cfe5d
Aaron Knauf <aknauf@ebay.com>
2013-07-09 08:55:25 +0100

    Added copyright/license notice under Apache v2.0

--- src/test/adk/nolog/NoLogTest.java
+++ src/test/adk/nolog/NoLogTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Aaron Knauf
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package adk.nolog;
 
 
@@ -8,12 +24,10 @@ import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static adk.nolog.LogLevel.ERROR;
-import static java.util.logging.Level.FINEST;
-import static java.util.logging.Level.SEVERE;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
+import static adk.nolog.LogLevel.*;
+import static java.util.logging.Level.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class NoLogTest {
 
